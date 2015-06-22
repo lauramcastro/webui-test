dcb6611835b872aa62c25e1560aa2c0a049dfc12
Laura M. Castro <lcastro@udc.es>
2015-06-22 19:34:21 +0200

    Added includes folder.

---
+++ include/webui.hrl
@@ -0,0 +1,11 @@
+%%% -*- coding: utf-8 -*-
+%%% @author Laura M. Castro <lcastro@udc.es>
+%%% @copyright 2015
+%%% @doc
+%%%     Generic model for web-based GUIs, implemented as a behaviour
+%%%     (header file).
+%%% @end
+%%%-------------------------------------------------------------------
+
+-define(CHROMEDRIVER, "http://localhost:9515/").
+-define(SESSION, webui_test).
